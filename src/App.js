62c8b8d3ac3f9a4da740f9ba2749672462ed3173
Arsentii Tatarenko <tatarenkoarsen@gmail.com>
2021-11-14 09:44:17 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Counter from "./components/Counter";
+import React, { Component } from "react";
+import Tea from "./components/Tea";
+
+function App() {
+  return (
+    <>
+      <Tea />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/IceCream/index.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+function IceCream() {
+  const [temperature, setTemperature] = useState(0);
+  const [type, setType] = useState("Fruit Ice");
+  return (
+    <div>
+      <p>Мороженое</p>
+      <p>Температура {temperature}</p>
+      <p>Вид {type}</p>
+    </div>
+  );
+}
+
+export default IceCream;
